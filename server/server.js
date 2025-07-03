42e74e15f6f3cd9cb6f09ff49dd76f2700fb2703
Md Abu Bokar <mdabu0212@gmail.com>
2025-07-04 00:35:12 +0600

    api test

--- server/server.js
+++ server/server.js
@@ -2,3 +2,13 @@ import express from "express"
 import connectDB from "./db/database.js"
 
 connectDB()
+
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.send("Working")
+})
+
+app.listen(8000, (err)=>{
+    console.log('Port listening on 8000')
+})
